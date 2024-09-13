Archived: Add monocular visual ranging, alert thread, fix velocity and brake_a calculation inaccuracy

Main/main.py
@@ -1,4 +1,4 @@
-import os,argparse,sys,traceback,time
+import os,argparse,sys,traceback,time,subprocess
 import rich,cv2
 import numpy as np
 from queue import Queue
@@ -13,9 +13,6 @@ from reactor import Reactor
 from model import Yolo8_Detracker
 from utils import DataSource, LoadRsCamera
 
-# todo:
-# 1. multi-thread for alert
-
 def update_args(args):
     config = yaml_load(args.config) # dict
     for key, value in config.items():
@@ -23,22 +20,23 @@ def update_args(args):
     return args
 
 def terminate_handle(sig, frame, 
-                     producer_thread, consumer_thread, 
-                     q, terminator):
+                     producer_thread, consumer_threads, 
+                     queues, terminator):
     print('\nSignal received, waiting for the completion of current batch ...')
     
     terminator.set()
 
     producer_thread.join() # waiting for yolo8 model to finish the last batch
     
-    if q is not None:
-        q.put("EOP") # send end-of-process signal to consumer thread
-        consumer_thread.join()
+    for queue,thread in zip(queues,consumer_threads):
+        if queues is not None:
+            queue.put("EOP") # send end-of-process signal to consumer thread
+            thread.join()
     
     print('Exiting.')
     sys.exit(0)
 
-def main(args, terminator, res_queue=None):
+def main(args, terminator, res_queue=None, alert_queue=None):
     try:
         datas = DataSource(args)
         args.if_track = getattr(args, "enable_track", \
@@ -57,31 +55,31 @@ def main(args, terminator, res_queue=None):
             
             for path_bs, img_bs, info_bs in datas: # *_bs are list with a len of args.batch
                 if terminator.is_set(): # terminate the program when encounter KeyBoardInterrupt
-                        break
+                    break
                 
-                res_id = 0
-                for res in model(path_bs, img_bs):  # batch_res is a list of Frame_Info objs
+                for res_id, res in enumerate(model(path_bs, img_bs)):  # batch_res is a list of Frame_Info objs
                     frame_emergent_data = res.emergency_info # ndarray, (num_boxes, 4), [cls, track_id, light_color, depth]
-                    v0, brake_a, if_alert = reactor.update(frame_emergent_data, res.fps) # unit: km/h, m/s^2
+                    v0, brake_a, if_alert, beep_info = reactor.update(frame_emergent_data, res.fps) # unit: km/h, m/s^2
                     
                     res.v0 = v0 # unit: km/h
                     res.brake_a = brake_a
                     res.if_alert = if_alert
 
+                    if alert_queue is not None:
+                        alert_queue.put(beep_info)
+
                     if res_queue is not None:
                         res_queue.put(res)
                     
                     if args.verbose:
-                        info = [info_bs[res_id], res.verbose()]
-                        rich.print(f"[bold][blue]{info[0]}[/blue][green]{info[1]}[/green][/bold]")
+                        info = [info_bs[res_id], res.verbose(), ' ⚠' if if_alert else '']
+                        rich.print(f"[bold][blue]{info[0]}[/blue][green]{info[1]}[/green][red]{info[2]}[/red][/bold]")
                         if res_queue is not None:
-                            res_queue.put(''.join(info))
+                            res_queue.put(''.join(info).strip())
                     
                     if args.show:
                         res.plot(show=True)
                     
-                    res_id += 1
-
     except:
         print(traceback.format_exc())
     
@@ -91,8 +89,9 @@ def main(args, terminator, res_queue=None):
         if isinstance(datas.data_iter,LoadRsCamera):
             datas.data_iter.pipeline.stop()
                 
-        if res_queue is not None:
-            res_queue.put('EOP') # end of process
+        for queue in (alert_queue, res_queue):
+            if queue is not None:
+                queue.put('EOP') # end of process
         
         for _ in range(2): # ring twice meaning program ends
                 os.system('play --no-show-progress --null --channels 1 synth 0.1 sine 1000')
@@ -100,9 +99,20 @@ def main(args, terminator, res_queue=None):
         #     if isinstance(v, cv2.VideoWriter):
         #         v.release()
 
+def alert(alert_queue):
+    if_speaker = subprocess.getoutput("aplay -l | grep -i usb") # check if speaker is connected
+    while True:
+        beep_info = alert_queue.get() # (duration, frequency) or None or 'EOP'
+        if beep_info == 'EOP': # end of process
+            break
+
+        if beep_info is not None and if_speaker:
+            duration, frequency = beep_info
+            if duration*frequency != 0:
+                os.system(f'play --no-show-progress --null --channels 1 synth {duration} sine {frequency}')
+
 def save_results(args, res_queue):
     count = 0
-    infos = []
     img_save_path = os.path.join(args.save_dir, 'img')
     meta_save_path = os.path.join(args.save_dir, 'meta')
     os.makedirs(img_save_path, exist_ok=True)
@@ -110,28 +120,25 @@ def save_results(args, res_queue):
 
     img_saving_mode = getattr(args, "save_img_format", 'gray').lower()
 
-    while True:
-        res = res_queue.get()
-        if res == 'EOP': # end of process
-            break
-        
-        if isinstance(res, str): # info
-            infos.append(res+'\n')
-        else:   # Frame_Info obj saved as pkl
-            save_name = os.path.splitext(os.path.basename(res.path))[0]
-            if not args.input.isnumeric():
-                save_name = f'{save_name}_{count}'
-
-            img, meta_data = res.compact_data
-            cv2.imwrite(os.path.join(img_save_path, save_name+'.png'), img if img_saving_mode == 'rgb' else img[...,0])
-            np.save(os.path.join(meta_save_path, save_name+'.npy'), meta_data)
-        
-            count += 1 
-    
-    if infos:
-        with open(os.path.join(args.save_dir, 'terminal_output.txt'), 'w') as info_writer:
-            info_writer.writelines(infos)
+    with open(os.path.join(args.save_dir, 'terminal_output.txt'), 'w') as info_writer:
+        while True:
+            res = res_queue.get()
+            if res == 'EOP': # end of process
+                break
             
+            if isinstance(res, str): # info
+                info_writer.write(res+'\n')
+            else:   # Frame_Info obj saved as pkl
+                save_name = os.path.splitext(os.path.basename(res.path))[0]
+                if not args.input.isnumeric():
+                    save_name = f'{save_name}_{count}'
+
+                img, meta_data = res.compact_data
+                cv2.imwrite(os.path.join(img_save_path, save_name+'.png'), img if img_saving_mode == 'rgb' else img[...,0])
+                np.save(os.path.join(meta_save_path, save_name+'.npy'), meta_data)
+            
+                count += 1 
+                
     print(f'Capture {count} frames in total.')
 
 if __name__ == '__main__':
@@ -161,13 +168,17 @@ if __name__ == '__main__':
         os.makedirs(args.save_dir, exist_ok=True)
         
     res_queue = Queue() if args.save_dir else None
+    alert_queue = Queue()
     terminator = Event()
 
-    main_thread = Thread(target=main, args=(args, terminator, res_queue))
+    main_thread = Thread(target=main, args=(args, terminator, res_queue, alert_queue))
     save_thread = Thread(target=save_results, args=(args, res_queue)) if args.save_dir else None
+    alert_thread = Thread(target=alert, args=(alert_queue,))
     signal(SIGINT, partial(terminate_handle,  # capture KILL (Ctrl+C) signal and stop the program
-                           producer_thread=main_thread, consumer_thread=save_thread,
-                           q=res_queue, terminator=terminator))
+                           producer_thread=main_thread, 
+                           consumer_threads=[save_thread,alert_thread] if args.save_dir else [alert_thread],
+                           queues=[res_queue,alert_queue] if args.save_dir else [alert_queue], # seqence should be the same as consumer_threads
+                           terminator=terminator))
     
     rich.print(Panel(Text("Press Ctrl+C to stop the program!", justify="center", style='bold green italic'), 
                      title="Note",
@@ -175,10 +186,12 @@ if __name__ == '__main__':
                      border_style='green'))
 
     main_thread.start()
+    alert_thread.start()
     if args.save_dir:
         save_thread.start()
 
     main_thread.join()
+    alert_thread.join()
     if args.save_dir:
         save_thread.join()
 

Main/model.py
@@ -1,4 +1,5 @@
 import os, torch
+import numpy as np
 from ultralytics.utils import ops
 from ultralytics.trackers.bot_sort import BOTSORT
 from ultralytics.engine.predictor import BasePredictor
@@ -81,7 +82,10 @@ class Yolo8_Detracker(BasePredictor): # yolov8 detector or tracker
 
         args.batch = self.args.batch # update batch size according to model args
         self.camera_flag = 1 if args.input.isnumeric() else 0 # used to receive depth information
+        self.enable_depth= getattr(args, "enable_depth", False)
         self.det_analyse_settings = getattr(args, "det_analyse_settings", dict())
+        self.det_analyse_settings["intri_mat"] = np.asanyarray(getattr(args, "cs_intrimat", \
+                                                               self.det_analyse_settings.get("intri_mat", None))) # update intri_mat with realsense camera built-in value
 
         self.callback = Track_Callback(args, self, persist=True) if args.if_track else Callback_Base()
     
@@ -110,7 +114,7 @@ class Yolo8_Detracker(BasePredictor): # yolov8 detector or tracker
                                        preds,  
                                        im, 
                                        imgs, 
-                                       depth_imgs=depth_imgs if self.camera_flag else None)
+                                       depth_imgs=depth_imgs if self.camera_flag and self.enable_depth else None)
             self.callback.on_predict_postprocess_end(im0s=imgs, # update tracker
                                                      results=results) 
         

Main/reactor.py
@@ -54,7 +54,7 @@ class Reactor():
                 brake_a = 0 # unit: m/s^2
             
             self.frame_v0.append(v0)
-            if_alert = self.alert(brake_a)
+            if_alert, *beep_info = self.alert(brake_a) # beep_info: (duration, frequency)
             
             # discard the very first frame's data
             key_ls = list(self.frame_dis.keys())
@@ -66,9 +66,10 @@ class Reactor():
         else:
             v0, brake_a = np.nan, 0
             if_alert = False
+            beep_info = None
             self.record_idx += 1 # while not reach win_size, just keep storing the frame data
-        
-        return v0*3.6, brake_a, if_alert  # unit: km/h, m/s^2
+
+        return v0*3.6, brake_a, if_alert, beep_info  # unit: v0(km/h), brake_a(m/s^2), beep_info: (duration(second), frequency(Hz))
 
     def get_v0(self, dis_mat):
 
@@ -91,7 +92,9 @@ class Reactor():
             v0 = v0 if a <= self.max_a else last_frame_v0+self.max_a*self.frame_fps[-1]*(v0-last_frame_v0)
             v0 = max(0,v0)
 
-        return v0 # unit: m/s
+        v0 = 7.7-42*v0 if v0*420 > 7 else v0*420 # unit: km/h
+
+        return v0 # unit: m/s, 60 is empirical scale
             
     def get_brake_a(self, track_dis, v0, vt, light_color):
         '''
@@ -133,8 +136,8 @@ class Reactor():
     def alert(self, brake_a):
         degree = brake_a/self.max_a
         if_alert = degree > self.alert_thres 
-        if if_alert:
-            duration = max(5/self.frame_fps[-1], 0.1) # second
-            frequency = int(880 * (1 + degree)) # Hz
-            os.system(f'play --no-show-progress --null --channels 1 synth {duration:.2f} sine {frequency}') # sudo apt install sox
-        return if_alert
+
+        duration = max(5/self.frame_fps[-1], 0.1) if if_alert else 0 # second
+        frequency = int(880 * (1 + degree)) if if_alert else 0 # Hz
+        #os.system(f'play --no-show-progress --null --channels 1 synth {duration:.2f} sine {frequency}') # sudo apt install sox
+        return if_alert, duration, frequency

Main/utils.py
@@ -12,10 +12,14 @@ class LoadRsCamera():
         assert hasattr(args, "streams"), f"Please specify `streams` in {args.config}"
         
         self.config = rs.config()
+        self.enable_depth = False
+
         # enable stream defined in yaml file
         for stream_name, stream_args in args.streams.items():
             stream_args = {key: eval(value) for key, value in stream_args.items()}
             self.config.enable_stream(**stream_args)
+            if stream_name == "depth":
+                self.enable_depth = True
         
         self.pipeline = rs.pipeline()
         self.profile = self.pipeline.start(self.config)
@@ -24,7 +28,12 @@ class LoadRsCamera():
         self.device_name = device_metadata.get_info(rs.camera_info.name)
         self.depth_sensor,self.color_sensor = device_metadata.query_sensors()
         self.depth_unit = self.depth_sensor.get_option(rs.option.depth_units) # Number of meters represented by a single depth unit
-        
+        cs_intrimat_meta = self.profile.get_stream(rs.stream.color).as_video_stream_profile().get_intrinsics()
+        args.cs_intrimat = np.array([[cs_intrimat_meta.fx, 0, cs_intrimat_meta.ppx],        # fx, 0, u0
+                                     [0, cs_intrimat_meta.fy, cs_intrimat_meta.ppy],        #  0, fy,v0
+                                     [cs_intrimat_meta.height, cs_intrimat_meta.width, 1]]) #  h, w, 1
+        args.enable_depth = self.enable_depth
+
         self.filters = {}
         if hasattr(args, "advnc_args"):
             self.advance_tune(args.advnc_args)
@@ -60,23 +69,23 @@ class LoadRsCamera():
         state = True
         
         # align depth img to color img
-        aligned_frames = self.align.process(raw_frame)
+        aligned_frames = self.align.process(raw_frame) if self.enable_depth else raw_frame
         color_part = aligned_frames.get_color_frame()
         depth_part = aligned_frames.get_depth_frame()
 
-        if not color_part or not depth_part:
+        if not color_part or (not depth_part and self.enable_depth):
             state = False
             return state, None
 
         # post process
         if 'color' in self.filters:
             color_part = self.apply_filters_ls(color_part, self.filters['color'])
-        if 'depth' in self.filters:
+        if 'depth' in self.filters and self.enable_depth:
             depth_part = self.apply_filters_ls(depth_part, self.filters['depth'])
         
         color_frame = np.asanyarray(color_part.get_data())
-        depth_frame = np.asanyarray(depth_part.get_data())
-        depth_frame = depth_frame * self.depth_unit # mm -> m
+        depth_frame = np.asanyarray(depth_part.get_data()) if depth_part else None
+        depth_frame = depth_frame * self.depth_unit  if depth_frame is not None else None # mm -> m
 
         return state, color_frame, depth_frame
 
@@ -205,6 +214,15 @@ class Frame_Info(Results):
                                     'Green': ([35,43,46], [89,255,255]),
                                     'Yellow': ([11,43,46], [34,255,255])}
         
+        # get monocular vision ranging related info defined in yaml file
+        self.camera2ground = settings.get("camera2ground", 1.3)
+        self.camera2front = settings.get("camera2front", 1.8)
+        self.standard_light_width = settings.get("standard_light_width", 0.35)
+        self.standard_human_height = settings.get("standard_human_height", 1.64)
+        self.intri_mat = settings["intri_mat"] if depth_img is None else None # depth image first
+        self.calibrated_frame_height = (self.intri_mat[2,0] if self.intri_mat is not None else None) \
+                                        or settings.get("calibrated_frame_height", 1080)
+
         frame_h, frame_w = self.orig_shape
         h_margin = settings.get("h_margin", 0.013)
         w_margin = settings.get("w_margin", 0.1)
@@ -234,8 +252,6 @@ class Frame_Info(Results):
 
         # get depth data
         det = boxes[:,:-2].copy() 
-        self.depth = self.get_box_depth(det, self.depth_img) if self.depth_img is not None \
-                     else np.array([2000]*num_boxes) # unit: meter
         
         # filt out neglect boxes
         center_x = (det[:,0]+det[:,2])/2
@@ -246,22 +262,29 @@ class Frame_Info(Results):
         # filt out emergency box
         frame_cls = set(det[:,-1])
         emergency_idx = []
-        if 2000 in self.depth: # depth data invalid, filt by box loc 
-            for cls in frame_cls:
-                # pick the lowest box for people and zebra-crossing, but highest for traffic light
-                pick_func = min if self.names[cls] == "T" else max 
-                cls_idx = np.where(det[:,-1] == cls)[0]
-                emergency_idx.append(pick_func(cls_idx, key=lambda x: det[x,3]))
-        else: # depth data valid, filt by depth
-            ascending_dis_idx = self.depth.argsort()
-            for idx in ascending_dis_idx:
-                if idx in grey_objs_idx: continue
-                if len(frame_cls) == 0: break
-                box_cls = det[idx,-1] 
-                if box_cls in frame_cls:
-                    emergency_idx.append(idx)
-                    frame_cls.remove(box_cls)
+        for cls in frame_cls:
+            # pick the lowest box for people and zebra-crossing, but highest for traffic light
+            pick_func = min if self.names[cls] == "T" else max 
+            cls_idx = np.where(det[:,-1] == cls)[0]
+            emergency_idx.append(pick_func(cls_idx, key=lambda x: det[x,3]))
+        # if 2000 in self.depth: # depth data invalid, filt by box loc 
+        #     for cls in frame_cls:
+        #         # pick the lowest box for people and zebra-crossing, but highest for traffic light
+        #         pick_func = min if self.names[cls] == "T" else max 
+        #         cls_idx = np.where(det[:,-1] == cls)[0]
+        #         emergency_idx.append(pick_func(cls_idx, key=lambda x: det[x,3]))
+        # else: # depth data valid, filt by depth
+        #     ascending_dis_idx = self.depth.argsort()
+        #     for idx in ascending_dis_idx:
+        #         if idx in grey_objs_idx: continue
+        #         if len(frame_cls) == 0: break
+        #         box_cls = det[idx,-1] 
+        #         if box_cls in frame_cls:
+        #             emergency_idx.append(idx)
+        #             frame_cls.remove(box_cls)
         boxes[emergency_idx, -2] = -1 # set box color to red idx, which means this box is emergency
+
+        self.depth = self.get_box_depth(det, emergency_idx, self.intri_mat, self.depth_img) # unit: meter
 
         # fill color and traffic light color information for each box
         for idx, box_data in enumerate(boxes):
@@ -279,25 +302,56 @@ class Frame_Info(Results):
         # update boxes data
         self.update(boxes=boxes)
 
-    def get_box_depth(self, preds, depth_img):
+    def get_box_depth(self, preds, emergency_idx, intri_mat=None, depth_img=None):
         '''
-        Return the average depth value of 3 points on the box's centerline as final depth value
+        Return the each box's depth as a vector
 
         preds: tensor, (num_boxes,6), column meanings are [x1, y1, x2, y2, confidence, class]
-        depth_imgs: ndarray, (H,W). Depth data(unit m)
+        intri_mat: ndarray(3,3). Intrinsic matrix of camera, [[fx,0,u0],
+                                                              [0,fy,v0],
+                                                              [h,w,1]]
+        depth_imgs: ndarray(H,W) or None. Depth data(unit m) captured by realsense camera
         '''
-        x1, y1, x2, y2 = preds[:,:4].T.astype(np.uint8) # x*,y* are vectors with shape (num_boxes,)
-        x1 = np.clip(x1, 0, depth_img.shape[1]-1)
-        x2 = np.clip(x2, 0, depth_img.shape[1]-1)
-        y1 = np.clip(y1, 0, depth_img.shape[0]-1)
-        y2 = np.clip(y2, 0, depth_img.shape[0]-1)
+        x1, y1, x2, y2 = preds[:,:4].T.astype(np.int16) # x*,y* are vectors with shape (num_boxes,)
+        x1 = np.clip(x1, 0, self.orig_shape[1]-1)
+        x2 = np.clip(x2, 0, self.orig_shape[1]-1)
+        y1 = np.clip(y1, 0, self.orig_shape[0]-1)
+        y2 = np.clip(y2, 0, self.orig_shape[0]-1)
         
-        top_points = y1, (x1+x2)//2
-        mid_points = (y1+y2)//2, (x1+x2)//2
-        buttom_points = y2, (x1+x2)//2
+        if depth_img is not None: # for depth camera
+            top_points = y1, (x1+x2)//2
+            mid_points = (y1+y2)//2, (x1+x2)//2
+            buttom_points = y2, (x1+x2)//2
+            
+            depth_vals = np.c_[depth_img[top_points], depth_img[mid_points], depth_img[buttom_points]]
+            depth_vals = np.mean(depth_vals, axis=1)
         
-        depth_vals = np.c_[depth_img[top_points], depth_img[mid_points], depth_img[buttom_points]]
-        return np.mean(depth_vals, axis=1)
+        elif intri_mat is not None: # Monocular vision ranging
+            fy,v0 = intri_mat[1, 1:]
+            frame_h = self.orig_shape[0]
+
+            box_width = x2-x1
+            box_height = y2-y1
+            depth_vals = np.full_like(preds[:, -1], 2000, dtype=np.float32)
+            for box_idx, box_cls in enumerate(preds[:, -1]):
+                if box_idx not in emergency_idx:
+                    continue
+                if self.names[box_cls] == "T": # traffic light
+                    criterion = min(box_width[box_idx], box_height[box_idx]) * self.calibrated_frame_height / frame_h # use the shortest edge to calculate dis
+                    depth_vals[box_idx] = max(fy * self.standard_light_width / criterion - self.camera2front, 0.1) 
+                
+                elif self.names[box_cls] == "P": # person
+                    criterion = box_height[box_idx] * self.calibrated_frame_height / frame_h # use the height of person to calculate dis
+                    depth_vals[box_idx] = max(fy * self.standard_human_height / criterion - self.camera2front, 0.1) # unit meter, default person height is 1.64m
+            
+                else: # zebra crossing
+                    depth_vals[box_idx] = max(0.1, fy * self.camera2ground / (abs(y2[box_idx] - frame_h // 2) * self.calibrated_frame_height / frame_h) - self.camera2front) # unit meter
+            depth_vals *= 0.5 # 0.5 is the empirical scale
+        
+        else:
+            depth_vals = np.array([2000]*len(preds))
+
+        return depth_vals
 
     def get_light_color(self, roi:np.ndarray):
         '''